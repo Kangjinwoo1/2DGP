4f0bf7212686eeb0b2c8f973b4f02842b6b184e6
Kangjinwoo1 <kjw955486@gmail.com>
2015-10-27 20:48:11 +0900

    코드
    코드

--- 2DGP Project/Bombman & Road.py
+++ 2DGP Project/Bombman & Road.py
@@ -15,8 +15,29 @@ class Road:
 
     def draw(self):
         global road_y
-        self.image.draw(400, 500 + road_y)
+        self.image.draw(400, 540 + road_y)
 
+class Truck:
+
+    def __init__(self):
+        self.image = load_image('truck.png')
+
+    def draw(self):
+        global road_y
+        global truck_x, truck_x2
+        self.image.draw(truck_x, 321 + road_y)
+        self.image.draw(-100 + truck_x2, 492 + road_y)
+
+class Redcar:
+
+    def __init__(self):
+        self.image = load_image('redcar.png')
+
+    def draw(self):
+        global road_y
+        global redcar_x
+        self.image.draw(800 + redcar_x, 398 + road_y)
+        self.image.draw(1000 + redcar_x2, 564 + road_y)
 
 class Bombman:
 
@@ -52,10 +73,10 @@ class Bombman:
             Bombman.image = load_image('bombman_animation.png')
 
     def draw(self):
-        global road_y
+        global bombman_y
         global x
         global y
-        self.image.clip_draw(self.frame * 100, 0, 100, 100, x + 400, y + 210 + road_y)
+        self.image.clip_draw(self.frame * 100, 0, 100, 100, x + 400, y + 210 + bombman_y)
 
 
 def handle_events():
@@ -90,27 +111,29 @@ def main():
 
     bombman = Bombman()
     road = Road()
+    truck = Truck()
+    redcar = Redcar()
 
     global running
     running = True
-    global x
-    x = 0
-    global y
-    y = 0
-    global road_y
-    road_y = 0
-    global walk_state
-    walk_state = 0
+    global x, y, road_y, bombman_y, car_y, walk_state, truck_x, truck_x2, redcar_x, redcar_x2
+    x, y, road_y, bombman_y, car_y, walk_state, truck_x, truck_x2, redcar_x, redcar_x2 = 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
 
     while running:
         handle_events()
-
+        bombman_y -= 3
         road_y -= 3
+        truck_x += 10
+        truck_x2 += 5
+        redcar_x -= 8
+        redcar_x2 -= 12
         bombman.update()
 
         clear_canvas()
 
         road.draw()
+        truck.draw()
+        redcar.draw()
         bombman.draw()
 
         update_canvas()
